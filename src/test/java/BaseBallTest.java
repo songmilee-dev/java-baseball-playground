faeef68b49b2f5bf6c1b6867bbd5c6f2875e8ea3
songmilee <lsm941010@gmail.com>
2023-06-17 17:11:21 +0900

    test: 정답 여부, 문자열 빈 값 여부 확인

--- src/test/java/BaseBallTest.java
+++ src/test/java/BaseBallTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BaseBallTest {
     final String defaultAnswer = "123";
@@ -37,6 +38,18 @@ public class BaseBallTest {
         int ballCnt = baseBall.countBall(oneBallInput);
 
         assertThat(ballCnt).isEqualTo(1);
+    }
+
+    @Test
+    void isCorrectAnswer() {
+        assertThat(baseBall.isCorrectAnswer(defaultAnswer)).isTrue();
+        assertThat(baseBall.isCorrectAnswer("456")).isFalse();
+    }
+
+    @Test
+    void isStringNotEmpty() {
+        assertThat(baseBall.isStringNotEmpty("")).isFalse();
+        assertThat(baseBall.isStringNotEmpty("133")).isTrue();
     }
 
     private Boolean isDuplicateNumber(String value) {
