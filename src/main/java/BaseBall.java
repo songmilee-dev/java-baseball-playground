9c45aecb4c681d81ab9a3282e728af442b0106a0
songmilee <lsm941010@gmail.com>
2023-06-17 16:56:44 +0900

    feat: 정답 생성

---
+++ src/main/java/BaseBall.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class BaseBall {
+
+    final int SIZE = 3;
+
+    private String answer;
+    String generateAnswer(String defaultAnswer) {
+        if(isStringNotEmpty(defaultAnswer)) {
+            return defaultAnswer;
+        }
+
+        // 1 ~ 9 까지 숫자 생성
+        Set<Integer> randomNumbers = new HashSet<>();
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+
+        while(randomNumbers.size() < SIZE) {
+            int randomValue = random.nextInt(8) + 1;
+            if(!randomNumbers.contains(randomValue)) {
+                randomNumbers.add(randomValue);
+                sb.append(randomValue);
+            }
+        }
+        return sb.toString();
+    }
+}
